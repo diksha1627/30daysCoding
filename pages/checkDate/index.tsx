174d25cb70b5989948e5ed4d20c01ab02658f8db
Diksha <dikshabuxani84745@gmail.com>
2023-08-27 23:55:36 +0530

    due date changes error fixed

--- pages/checkDate/index.tsx
+++ pages/checkDate/index.tsx
@@ -1,29 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
-
+// Define an interface for the expected data structure
+interface ApiResponse {
+    msg: string;
+    daysLeft: number;
+    next_payment_date_in_datetime: string; // Update this with the correct type
+    // ... Other properties you expect in the API response
+}
 
 const Index = () => {
-
     const [email, setEmail] = useState('');
-    const [data, setData] = useState('');
-
+    const [data, setData] = useState<ApiResponse | null>(null); // Use the ApiResponse interface
 
     const handleSubmit = async (e: any) => {
         e.preventDefault();
-        await axios.post("/api/data", { email: email })
-            .then(response => {
-                setData(response.data); // Assuming your response has a data property
-            })
-            .catch(error => {
-                console.error("Error fetching data:", error);
-            });
+        try {
+            const response = await axios.post("/api/data", { email: email });
+            setData(response.data);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
     }
 
-    const nextPaymentDate = new Date(data.next_payment_date_in_datetime);
+    // Make sure to handle cases where data is null or undefined
+    const nextPaymentDate = data ? new Date(data.next_payment_date_in_datetime) : new Date();
 
-    const options = { year: 'numeric', month: 'long', day: 'numeric', timeZone: 'UTC' };
+    const options: Intl.DateTimeFormatOptions = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        timeZone: 'UTC'
+    };
     const formattedDate = nextPaymentDate.toLocaleDateString('en-US', options);
+
     return (
         <div className="w-full ml-12 lg:ml-64 md:ml-64 mt-20 lg:p-10">
 
@@ -46,7 +56,7 @@ const Index = () => {
                 </form>
 
                 <div className="mt-4 flex-col p-2  justify-center items-center border-2">
-                <p className='text-[20px]'><span className='font-bold text-[22px] font-times'> Days Left : </span>{data.msg=="email id not found" ? "Invalid Email" : data.daysLeft} </p>
+                <p className='text-[20px]'><span className='font-bold text-[22px] font-times'> Days Left : </span>{data?.msg=="email id not found" ? "Invalid Email" : data?.daysLeft} </p>
                 <p className='text-[20px]'><span className='font-bold text-[22px] font-times'>Next Payment Date : </span> {data && formattedDate}</p>
                     
                     </div>  
